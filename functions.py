adf3de641574ad9302599bd3594e0806a7a95023
AchmadBauravindah <baurav99@gmail.com>
2023-06-19 14:07:17 +0700

    ok

--- functions.py
+++ functions.py
@@ -132,7 +132,7 @@ def saveSegmentation():
     used_model = st.session_state.used_model
     if used_model == 'All Models At Once':
         models = st.session_state.existing_models.keys()
-        for model_name in models.keys():
+        for model_name in models:
             model = tf.keras.models.load_model('./models/{}/{}.h5'.format(model_name, model_name), compile=False)
             for i, image_arr in enumerate(images_arr):
                 ori_image, mask_image, line_mask_image, merge_image, segmented_image = createSegmentation(model, image_arr)
